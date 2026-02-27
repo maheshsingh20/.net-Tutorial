3b396a3e2df7ed9cb0dc621d377724d48d823e34
Mahesh Singh <maheshsingh20@example.com>
2026-02-27 09:43:20 +0530

    Version v1

---
+++ ASP.NET_WEB_CORE_MVC/Ecommerce_Inventory/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_Inventory.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
